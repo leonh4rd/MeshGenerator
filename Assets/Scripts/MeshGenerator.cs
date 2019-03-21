30da18b0095703a5c582aaa8bac94976b2f746c0
Leonardo <leonardo@quetzal.com.br>
2019-03-21 15:02:07 -0300

    Modify script to build entire plane slowly

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -17,11 +17,13 @@ public class MeshGenerator : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        CreateMesh();
+        StartCoroutine(CreateMesh());
+    }
+    void Update() {
         UpdateMesh();
     }
 
-    void CreateMesh()
+    IEnumerator CreateMesh()
     {
         vertices = new Vector3[(xSize + 1)*(zSize + 1)];
         for(int i = 0, z = 0; z <= zSize; z++)
@@ -32,6 +34,26 @@ public class MeshGenerator : MonoBehaviour
                 i++;
             } 
         } 
+
+        int vert = 0;
+        int tris = 0;
+        triangles = new int[xSize*zSize*6];
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+                yield return new WaitForSeconds(.1f);
+            }   
+            vert++;
+        }
     }
 
     void UpdateMesh()
@@ -42,6 +64,7 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
+    /* 
     private void OnDrawGizmos() {
         if(vertices == null) 
             return;
@@ -50,4 +73,5 @@ public class MeshGenerator : MonoBehaviour
             Gizmos.DrawSphere(vertices[i], .2f);
         }    
     }
+    */
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MeshGenerator
 Generating a mesh by script inside Unity
+
+What it is:
+Following the simples tutorials from Brackeys on his channel on Youtube, this project generate mesh from script.
+The current version draws a plane of user defined dimensions on the XZ plane by creating the triangles and assigning them to a mesh.
+
+How it works:
+Simple open the project on Unity (version 2018.3+). Navigate to the folder Scenes and open SampleScene. Select the MeshGenerator object and
+set the dimension of the plane in the xSize and zSize fields of the MeshGenerator component. Hit Play and see the results. Currently it
+creates the triangles at pace of 0.2 seconds.
