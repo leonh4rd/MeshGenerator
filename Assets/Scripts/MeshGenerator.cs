9ae5c19415e7d21a7b03b614abd8c0c929c4b8e6
Leonardo <leonardo@quetzal.com.br>
2019-03-21 13:19:12 -0300

    Modify script to draw gizmos

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
+    public int xSize;
+    public int zSize;
+
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
@@ -20,19 +23,15 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateMesh()
     {
-        vertices = new Vector3[]
+        vertices = new Vector3[(xSize + 1)*(zSize + 1)];
+        for(int i = 0, z = 0; z <= zSize; z++)
         {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0),
-            new Vector3(1,0,1)
-        };    
-
-        triangles = new int[]
-        {
-            0, 1, 2,
-            1, 3, 2
-        };    
+            for(int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            } 
+        } 
     }
 
     void UpdateMesh()
@@ -41,5 +40,14 @@ public class MeshGenerator : MonoBehaviour
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos() {
+        if(vertices == null) 
+            return;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .2f);
+        }    
     }
 }
